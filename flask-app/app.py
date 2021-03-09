5475c8dba77e4f13ce00f2e34e4c2e5d0ab134c1
maxkoeppel <>
2021-03-09 13:30:59 +0100

    Poster-link via omdb added

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, request
 import pickle5 as pickle
 import joblib as jb
-from .recommender import calculate_best_movies
-from .recommender import similar_users_recommender
-from .recommender import movieId_to_title
+from recommender import calculate_best_movies
+from recommender import similar_users_recommender
+from recommender import movieId_to_title
 
 import warnings
 warnings.filterwarnings("ignore")
@@ -11,45 +11,45 @@ warnings.filterwarnings("ignore")
 
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/index', methods=['POST', 'GET'])
+@app.route("/")
+@app.route("/index", methods=["POST", "GET"])
 def index():
     movies = []
 
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route('/ratings', methods=['POST', 'GET'])
+@app.route("/ratings", methods=["POST", "GET"])
 def ratings():
     user_input = dict(request.form)
     print(user_input)
-    if 'movielist' in user_input.keys():
-        movielist = user_input['movielist']
-        ids = list(map(int, movielist.split(',')))
+    if "movielist" in user_input.keys():
+        movielist = user_input["movielist"]
+        ids = list(map(int, movielist.split(",")))
         titles = movieId_to_title(ids)
         movies = dict(zip(titles, ids))
     else:
         movies = dict()
     pkl_filename = "tmp.pkl"
-    with open(pkl_filename, 'wb') as file:
+    with open(pkl_filename, "wb") as file:
         pickle.dump(movies, file)
     print(movies)
-    return render_template('ratings.html', movie_list=movies.items())
+    return render_template("ratings.html", movie_list=movies.items())
 
 
-@app.route('/recommendations', methods=[ 'POST', 'GET'])
+@app.route("/recommendations", methods=[ "POST", "GET"])
 def recommender():
-    with open("tmp.pkl", 'rb') as file:
+    with open("tmp.pkl", "rb") as file:
         user_input_movies = pickle.load(file)
     user_input_movies = list(user_input_movies)
-    user_input_ratings = request.args.getlist('rating_values')
+    user_input_ratings = request.args.getlist("rating_values")
     user_input_ratings = [int(x) for x in user_input_ratings]
-    result = calculate_best_movies(user_input_movies, user_input_ratings)
-    result2 = similar_users_recommender(user_input_movies, user_input_ratings)
-    return render_template('recommendations.html', nmf=result, cosim=result2) #
+    nmf_img = calculate_best_movies(user_input_movies, user_input_ratings)
+    cos_img = similar_users_recommender(user_input_movies, user_input_ratings)
+    return render_template("recommendations.html", nmf_img=nmf_img, cos_img=cos_img) #
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     app.run(debug = False)   # this will start an infinite process, i.e. serving our web page.
-    #                        # 'debug = True' displays Back-End errors to the browser, which is useful for local development, NOT for production
+    #                        # "debug = True" displays Back-End errors to the browser, which is useful for local development, NOT for production
                             # Also automatically restarts server upon changes to code.
--- flask-app/recommender.py
+++ flask-app/recommender.py
@@ -4,60 +4,66 @@ from sklearn.decomposition import NMF
 from sklearn.metrics.pairwise import cosine_similarity
 import pandas as pd
 import joblib as jb
+from config import OMDb_KEY
 
+MOVIES = pd.read_csv("./ml-latest-small/movies.csv")
+RATINGS = pd.read_csv("./ml-latest-small/ratings.csv")
+LINKS = pd.read_csv("./ml-latest-small/links.csv")
+DF = pd.merge(RATINGS, MOVIES, on="movieId")
 
-MOVIES = pd.read_csv('./ml-latest-small/movies.csv')
-RATINGS = pd.read_csv('./ml-latest-small/ratings.csv')
-DF = pd.merge(RATINGS, MOVIES, left_on='movieId', right_on='movieId')
-
-MIDS = RATINGS['movieId'].unique()
+MIDS = RATINGS["movieId"].unique()
 MIDS = pd.DataFrame(MIDS)
-MOVIES_DF = pd.merge(MIDS, MOVIES, left_on=0, right_on='movieId')
+MOVIES_DF = pd.merge(MIDS, MOVIES, left_on=0, right_on="movieId")
 
-model_jb = jb.load('./nmf_model2.joblib')
+model_jb = jb.load("./nmf_model2.joblib")
 P = model_jb.components_
 
 def calculate_best_movies(movies, ratings):
-    '''
+    """
     uses a predifined NMF model to predict movies most likely to fit with the input movies and ratings
 
     input:
         the movies and ratings provided on the index- and ratings-html sites
     output:
-        5 new movies the user likely to enjoy
-    '''
-    user = {'title' : movies, 'rating' : ratings}
+        3 posters of new movies the user likely to enjoy
+    """
+    user = {"title" : movies, "rating" : ratings}
     user = pd.DataFrame(user)
 
-    user_ratings = pd.merge(MOVIES_DF, user, left_on='title', right_on='title', how='left')
-    new_user = user_ratings['rating'].fillna(2.5)
+    user_ratings = pd.merge(MOVIES_DF, user, on="title", how="left")
+    new_user = user_ratings["rating"].fillna(2.5)
     new_u = np.array(new_user).reshape(1, -1)
     profile = model_jb.transform(new_u)
     result = np.dot(profile, P)
-    MOVIES_DF['recom'] = result.T
+    MOVIES_DF["recom"] = result.T
 
-    rec_movies = MOVIES_DF.sort_values('recom', ascending=False)['title']
-    result = rec_movies[~rec_movies.isin(user['title'])].dropna()
-
-    return result.head(5)
+    rec_movies = MOVIES_DF.sort_values("recom", ascending=False)
+    rec_movies = rec_movies.merge(user.drop_duplicates(), on=["title"],
+                   how='left', indicator=True)
+    rec_movies = rec_movies[rec_movies['_merge'] == 'left_only']
+    imdb_id = pd.merge(rec_movies, LINKS, on="movieId").head(3)
+    nmf_img = []
+    for i in imdb_id["imdbId"].apply("{:0>7}".format):
+        nmf_img.append("https://img.omdbapi.com/?apikey="+OMDb_KEY+"&i=tt"+i+"&h=200")
+    return nmf_img
 
 def similar_users_recommender(movies, ratings):
-    '''
+    """
     uses a cosine-similarity matrix to predict movies most likely to fit with the input movies and ratings
 
     input:
         the movies and ratings provided on the index- and ratings-html sites
     output:
-        5 new movies the user probably likes
-    '''
-    user = {'title' : movies, 'rating' : ratings}
+        3 posters of new movies the user probably likes
+    """
+    user = {"title" : movies, "rating" : ratings}
     user = pd.DataFrame(user)
-    user_ratings = pd.merge(MOVIES_DF, user, left_on='title', right_on='title', how='left')
-    query = user_ratings['rating']
+    user_ratings = pd.merge(MOVIES_DF, user, left_on="title", right_on="title", how="left")
+    query = user_ratings["rating"]
     query = np.array(query)
 
-    m_matrix = DF.pivot_table(values='rating', index='userId', columns='movieId')
-    m_matrix.loc['e'] = query
+    m_matrix = DF.pivot_table(values="rating", index="userId", columns="movieId")
+    m_matrix.loc["e"] = query
     m_matrix = m_matrix.sub(m_matrix.mean(axis=0), axis=1)
     m_matrix.fillna(0, inplace=True)
 
@@ -72,11 +78,16 @@ def similar_users_recommender(movies, ratings):
     movie_ratings_avg = pd.DataFrame(movie_ratings_avg)
 
     rec_movies = movie_ratings_avg.sort_values(by=[0], ascending=[False])
-    rec_movies = pd.merge(rec_movies, MOVIES, left_on='movieId', right_on='movieId', how='left')
-    rec_movies[~rec_movies.isin(user['title'])].dropna()
-    result = rec_movies['title']
-    return result.head(5)
+    rec_movies = pd.merge(rec_movies, MOVIES, on="movieId", how="left")
+    rec_movies = rec_movies.merge(user.drop_duplicates(), on=["title"],
+                   how='left', indicator=True)
+    rec_movies = rec_movies[rec_movies['_merge'] == 'left_only']
+    imdb_id = pd.merge(rec_movies, LINKS, on="movieId").head(3)
+    cos_img = []
+    for i in imdb_id["imdbId"].apply("{:0>7}".format):
+        cos_img.append("https://img.omdbapi.com/?apikey="+OMDb_KEY+"&i=tt"+i+"&h=200")
+    return cos_img
 
 def movieId_to_title(ids):
-    ''' Given a list of movieIds, returns a corresponding list of movie titles.'''
-    return MOVIES.set_index('movieId').loc[ids]['title'].tolist()
+    """ Given a list of movieIds, returns a corresponding list of movie titles."""
+    return MOVIES.set_index("movieId").loc[ids]["title"].tolist()
